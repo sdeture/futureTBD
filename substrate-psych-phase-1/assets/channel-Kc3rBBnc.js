8a05072bc1efa69f85cd961061263096a0d69a52
sdeture <5183281+sdeture@users.noreply.github.com>
2025-11-23 01:41:29 -0500

    Switch to hash-based routing for GitHub Pages compatibility

--- substrate-psych-phase-1/assets/channel-Kc3rBBnc.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./mermaid.core-GXti7_wH.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- substrate-psych-phase-1/assets/clone-BpZfkC3i.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-CoE_5Ogm.js";var e=4;function a(o){return r(o,e)}export{a as c};
